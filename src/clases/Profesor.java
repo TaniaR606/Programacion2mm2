dced00abd65b6d77851f157978af7919a98a218e
TaniaR <>
2021-04-12 14:06:35 -0500

    Updated

---
+++ src/clases/Profesor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author lunit
+ */
+public class Profesor {
+     public String name;
+     public String lastn;
+     public String materia;
+    
+     
+public Profesor(String name,String lastn,String materia){
+        this.name = name;
+        this.lastn = lastn; 
+        this.materia= materia;
+}
+}
