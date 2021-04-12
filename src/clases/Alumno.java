9d70a9cc752e2aee6e0bc8097999ad034243aff1
TaniaR <lunit@DESKTOP-SH0K56H>
2021-04-12 14:03:48 -0500

    Updated

---
+++ src/clases/Alumno.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author lunit
+ */
+public class Alumno {
+   
+    public String nombre;
+    public String apellido;
+    public int edad;
+
+public Alumno(String nombre, String apellido, int edad){
+        this.nombre = nombre;
+        this.apellido = apellido; 
+        this.edad=edad;
+}
+
+    }
