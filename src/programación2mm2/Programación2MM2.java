43f338a7f5ca89a87bebc8307c40175318b8734d
lunit <lunit@DESKTOP-SH0K56H>
2021-04-12 13:43:10 -0500

    Updated

---
+++ src/programación2mm2/Programación2MM2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programación2mm2;
+        
+/**
+ *
+ * @author lunit
+ */
+public class Programación2MM2 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int g;
+       double h;
+       char n;
+       
+       
+      Fecha hoy = new Fecha(12, 04, 2021); 
+     // System.out.println("Hola mundo");
+        
+        Alumno yo= new Alumno("Tania","Ramos",19);
+        Animal no1= new Animal ("Perro", 'M', 12, "Si" );
+        Profesor No1= new Profesor("Roberto", "Cruz", "Programción");
+    
+    }
+    
+
+}
