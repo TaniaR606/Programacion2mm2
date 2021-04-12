5cc6d5378657f4a0086ffec07f7948877602a7f5
TaniaR <>
2021-04-12 14:05:59 -0500

    Updated

---
+++ src/clases/Animal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author lunit
+ */
+public class Animal {
+    
+    public String tipo;
+    public char sexo;
+    public int anios;
+    public String domestico;
+    
+public Animal(String tipo, char sexo, int anios,String domestico){
+        this.tipo = tipo;
+        this.sexo = sexo; 
+        this.anios= anios;
+        this.domestico=domestico;
+}
+
+}
---
+++ src/clases/Fecha.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author lunit
+ */
+public class Fecha {
+    
+  public  int dia;
+  public  int mes;
+  public  int anio;
+    
+  public Fecha(int dia, int mes, int anio){
+        this.dia = dia;
+        this.mes = mes;
+        this.anio= anio;
+  }
+  
+}
+
+
